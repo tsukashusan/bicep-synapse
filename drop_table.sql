608a5c268038a900ef7dbc8cae713102535ee091
tsukabon <shtsukam@outlook.com>
2021-08-31 09:02:26 +0900

    add drop script(SQL).

---
+++ drop_table.sql
@@ -0,0 +1,27 @@
+DROP TABLE [dbo].[仕入先]
+GO
+
+DROP TABLE [dbo].[受注]
+GO
+
+DROP TABLE [dbo].[社員]
+GO
+
+DROP TABLE [dbo].[都道府県]
+GO
+
+DROP TABLE [dbo].[商品]
+GO
+
+DROP TABLE [dbo].[受注明細]
+GO
+
+DROP TABLE [dbo].[得意先]
+GO
+
+DROP TABLE [dbo].[運送]
+GO
+
+DROP TABLE [dbo].[商品区分]
+GO
+
