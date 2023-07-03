6a82cef6a87e05efa7c29bd95d2c5d07db0c361d
tsukabon <shtsukam@outlook.com>
2023-07-03 11:29:19 +0900

    update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -24,13 +24,15 @@ set-variable -name TENANT_ID "xxxxxxxxxxxxxxxx" -option constant
 set-variable -name SUBSCRIPTOIN_GUID "xxxxxxxxxxxxxxxxx" -option constant
 set-variable -name BICEP_FILE "main.bicep" -option constant
 set-variable -name CONTAINER_NAME "dl2" -option constant
-set-variable -name SAMPLE_SOURCE "https://miscstrage.blob.core.windows.net/box/sample.zip?sv=2020-10-02&st=2022-07-05T10%3A50%3A43Z&se=2033-06-30T10%3A50%3A00Z&sr=b&sp=r&sig=NQAqp8i5LgVkUXyJ8M9%2BUYFKrKV86zj2VG8LBV9f2l8%3D"
+set-variable -name SAMPLE_SOURCE "https://storagezoxzpzrg3vzaw.blob.core.windows.net/distribution/sample.zip?sv=2021-12-02&st=2023-07-03T02%3A10%3A06Z&se=2026-07-04T02%3A10%3A00Z&sr=b&sp=r&sig=B%2BrnLJ%2FRHdm%2FRWRAlUCuzsiuGLNNm3m%2FrgrRbwQm3Ls%3D"
 
 $parameterFile = "azuredeploy.parameters.dev.json"
 $resourceGroupName = "rgxxxx"
 $location = "japaneast"
 
-Connect-AzAccount -Tenant ${TENANT_ID} -Subscription ${SUBSCRIPTOIN_GUID} -UseDeviceAuthentication
+# Connect-AzAccount -Tenant ${TENANT_ID} -Subscription ${SUBSCRIPTOIN_GUID} -UseDeviceAuthentication
+Connect-AzAccount -Tenant ${TENANT_ID} -Subscription ${SUBSCRIPTOIN_GUID}
+Set-AzContext -Tenant ${TENANT_ID} -Subscription ${SUBSCRIPTOIN_GUID} 
 
 New-AzResourceGroup -Name ${resourceGroupName} -Location ${location} -Verbose
 
